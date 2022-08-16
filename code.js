3cb2b4b14fb21103a8ff65ab4ea54de5243e723a
Nate Vemuri <nwvbug@gmail.com>
2022-08-15 18:01:54 -0700

    tnt gamer chagne

--- code.js
+++ code.js
@@ -60,7 +60,7 @@ function move() {
     i = 1;
     var elem = document.getElementById("myBar");
     var width = 1;
-    var id = setInterval(frame, 50);
+    var id = setInterval(frame, 15);
     function frame() {
       if (width >= 100) {
         clearInterval(id);
