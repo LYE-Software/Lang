47b891263069e596a3543c735144f4759f5129a9
Nate Vemuri <nwvbug@gmail.com>
2022-08-26 09:15:22 -0700

    styles

--- code.js
+++ code.js
@@ -963,10 +963,14 @@ function onBtnPress(v) {
         uploadFile.type = 'file';
         uploadFile.id = 'file';
         uploadFile.name = 'file';
+        uploadFile.style.width = "15vw";
+        uploadFile.style.marginRight = "2vw";
+
         uploadFile.accept = '.lang, .txt';
         document.body.appendChild(uploadFile);
         var uploadButton = document.createElement('button');
-        uploadButton.innerHTML = 'Upload';
+        uploadButton.innerHTML = 'Confirm upload';
+        uploadButton.className = "dropbtn";
         uploadButton.onclick = function() {
         var file = document.getElementById('file').files[0];
         var reader = new FileReader();
