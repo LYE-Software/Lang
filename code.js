530b2e809ffc263b5424572552a98744ae29c8a7
ankleBowl <specialreeree6@gmail.com>
2022-12-10 18:27:31 -0800

    Update code.js

--- code.js
+++ code.js
@@ -3015,3 +3015,20 @@ function createCreatorInput(term, definition) {
         var overallContainer = document.getElementById("langCreatorContainer");
         overallContainer.scrollTop = overallContainer.scrollHeight;
 }
+
+function addStudysheet(name) {
+    var sheet = `
+    <div class="studysheetGridContainer">
+        <div style="display: flex; align-items: center; background-color: var(--primary-dark);">
+            <div class="studysheetName">${name}</div>
+        </div>
+        <div style="display: flex; align-items: center; justify-content: center; background-color: var(--primary-dark);">
+            <svg class="studysheetEdit" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488.728 488.728" xmlns:v="https://vecta.io/nano"><path d="M487.147 462.52l-36.4-167.6c0-4.2-2.1-7.3-5.2-10.4l-261.3-261.3c-20-22.9-74.3-38.1-112.4 0l-47.9 47.9c-31 31-31 81.4 0 112.4l261.3 261.3c2.1 2.1 5.2 4.2 9.4 5.2l168.6 38.5c10.1 1.5 29.1-4.9 23.9-26zm-434.1-308.1c-15.6-15.6-15.6-39.6 0-55.2l47.9-47.9c15.2-15.2 40-15.2 55.2 0l238.4 238.4h-27.1c-11.4 0-20.8 9.4-20.8 20.8v34.3h-34.3c-11.4 0-20.8 9.4-20.8 20.8v26.1l-238.5-237.3zm280 261.3v-29.2h34.3c18 1.7 20.8-16.5 20.8-20.8v-34.4h29.2l24 109.3-108.3-24.9z"/></svg>
+        </div>
+        <div style="display: flex; align-items: center; justify-content: center; background-color: var(--primary-dark);">
+            <svg class="studysheetDelete" width="24px" height="24px" viewBox="0 0 24 24" fill="" xmlns="http://www.w3.org/2000/svg"><path d="M17 9L11 15M11.0002 9L17.0002 15M9 6H20C20.5523 6 21 6.44772 21 7V17C21 17.5523 20.5523 18 20 18H9L3 12L9 6Z" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg>
+        </div>
+    </div>
+    `
+    document.getElementById("studysheetGridContainer").innerHTML += sheet;
+}
