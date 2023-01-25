82702538bc9e24e878e92d854552db0feb6a8834
Nate Vemuri <nwvbug@gmail.com>
2023-01-24 22:23:58 -0800

    *sigh*

--- code.js
+++ code.js
@@ -3184,7 +3184,7 @@ function saveToCloud(){
                     if (xhr.readyState === 4) {
                         console.log(xhr.status);
                         console.log(xhr.responseText);
-                        //window.location.href="index.html";
+                        window.location.href="index.html";
                     }
                 };
                 var data = downloadArrayString;
