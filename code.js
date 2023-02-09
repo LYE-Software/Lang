af64dcdd5d84cab52b1e1f0f0f6f0ef7c309c5b1
Nate Vemuri <nwvbug@gmail.com>
2023-02-08 20:25:13 -0800

    no clue

--- code.js
+++ code.js
@@ -3062,7 +3062,7 @@ var trash_svg = `
 `
 
 var image_svg = `
-<svg width="100% height="100%" version="1.1" viewBox="0 0 700 700" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+<svg length="100% height="100%" version="1.1" viewBox="0 0 700 700" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
 
         
     <g>
