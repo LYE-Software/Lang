8933563bf471df6aad9435897a35cb61282d6c66
Nate Vemuri <nwvbug@gmail.com>
2023-10-03 23:11:54 -0700

    notifs

---
+++ javascript/Notification.js
@@ -0,0 +1,13 @@
+class Notification {
+    constructor(title, body, priority){
+        this.title = title; this.body = body; this.priority = priority;
+    }
+
+    getHTML(){
+        if (this.priority){
+            var html = `
+            
+            `
+        }
+    }
+}
